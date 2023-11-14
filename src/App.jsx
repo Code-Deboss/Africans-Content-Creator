d7272d467f57ee62ba9fe92107d86779e9974a24
Nwabuisi Obinna Divine <obinnadivine107@gmail.com>
2023-11-14 22:19:29 +0100

    working on content section navbar

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+import './App.css'
+import Dashboard from './dashboard/Dashboard'
+
+function App() {
+  return (
+    <div>
+      <Dashboard />
+    </div>
+  )
+}
+
+export default App
---
+++ src/dashboard/Dashboard.jsx
@@ -0,0 +1,22 @@
+import './styles/Dashboard.css'
+const Dashboard = () => {
+  return (
+    <div className="home">
+        <div className='sidebar'>
+
+        </div>
+        <div className="content">
+            <div className="content_navbar">
+                <div className="content_navbar_title">
+                    <h1>Dashboard</h1>
+                </div>
+                <div className="content_navbar_others">
+                    
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Dashboard
